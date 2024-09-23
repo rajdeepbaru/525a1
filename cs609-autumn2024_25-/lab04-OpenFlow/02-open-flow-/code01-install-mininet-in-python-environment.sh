9af18c974c706468af55fd0325c0460c03f54ccf
rajdeep <212011005@iitdh.ac.in>
2024-09-23 22:05:13 +0530

    planning-fflush

--- cs609-autumn2024_25-/lab04-OpenFlow/02-open-flow-/code01-install-mininet-in-python-environment.sh
+++ cs609-autumn2024_25-/lab04-OpenFlow/02-open-flow-/code01-install-mininet-in-python-environment.sh
@@ -1,3 +1,7 @@
 pip3 install mininet
 rm output01-static-flow.run
-sudo apt-get install vim
+#sudo apt-get install vim
+git clone https://github.com/mininet/mininet
+sudo mininet/util/install.sh -a
+mn --version
+
