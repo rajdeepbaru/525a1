82af8de43ee2e430f187ff22513ec5f71f577428
rajdeep <212011005@iitdh.ac.in>
2024-09-24 09:29:21 +0530

    backup-added-for-50G

---
+++ update-ubuntu-after-install.sh
@@ -0,0 +1,18 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install openssh-server
+sudo systemctl enable ssh
+
+sudo systemctl enable ssh --now
+sudo systemctl start ssh
+sudo apt-get install vim
+sudo apt-get install git
+
+echo "\n\n=========================\n\n"
+
+
+git clone git@github.com:rajdeepbaru/525a1.git
+
+curl -O https://codeload.github.com/rajdeepbaru/525a1/zip/refs/heads/main
+
+
