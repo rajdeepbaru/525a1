4d859e9c7af72807bece5f6b8624b8789f7203f8
rajdeep <212011005@iitdh.ac.in>
2024-09-27 14:33:42 +0530

    2024-09-27--1433

--- rm-dir-f-r-/update-ubuntu-after-install.sh
+++ rm-dir-f-r-/update-ubuntu-after-install.sh
@@ -9,14 +9,14 @@ sudo apt-get install vim
 sudo apt-get install git
 sudo apt-get install curl
 
-echo "\n\n=========================\n\n"
+echo "\n\nDone\n\n"
 
 
-rm -dir -f -r 525a1-main/
-curl -O https://codeload.github.com/rajdeepbaru/525a1/zip/refs/heads/main
-unzip main
-rm main
-cd 525a1-main/
+#rm -dir -f -r 525a1-main/
+#curl -O https://codeload.github.com/rajdeepbaru/525a1/zip/refs/heads/main
+#unzip main
+#rm main
+#cd 525a1-main/
 
 
 
