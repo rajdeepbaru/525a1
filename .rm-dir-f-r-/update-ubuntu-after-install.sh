087a9916f60ec3aace98c0b56428568a566fc9ec
rajdeep <212011005@iitdh.ac.in>
2024-09-29 12:27:45 +0530

    2024-09-29--1227

--- .rm-dir-f-r-/update-ubuntu-after-install.sh
+++ .rm-dir-f-r-/update-ubuntu-after-install.sh
@@ -1,15 +1,50 @@
 sudo apt-get update
+
+echo  -e "Done: update"
+
 sudo apt-get upgrade
+
+echo  -e "Done: upgrade"
+
+
 sudo apt-get install openssh-server
+
+echo  -e "Done: openssh-server installation"
+
+
 sudo systemctl enable ssh
 
+echo  -e "Done: ssh enabled"
+
+
+
 sudo systemctl enable ssh --now
+
+echo  -e "Done: ssh enabled now"
+
+
 sudo systemctl start ssh
+
+echo  -e "Done: ssh started"
+
+
 sudo apt-get install vim
+
+echo  -e "Done: vim innstalled"
+
+
 sudo apt-get install git
+
+echo  -e "Done: git installed"
+
+
 sudo apt-get install curl
 
-echo "\n\nDone\n\n"
+echo  -e "Done: curl installed"
+
+
+
+echo  -e "Done: all done"
 
 
 #rm -dir -f -r 525a1-main/
