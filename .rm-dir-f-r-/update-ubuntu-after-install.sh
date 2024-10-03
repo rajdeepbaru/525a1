fe2c31f6be2572303c6942066e0fdb9dced17a42
rajdeep <212011005@iitdh.ac.in>
2024-10-03 12:01:16 +0530

    2024-10-03--1201

--- .rm-dir-f-r-/update-ubuntu-after-install.sh
+++ .rm-dir-f-r-/update-ubuntu-after-install.sh
@@ -1,13 +1,13 @@
-sudo apt-get update
+sudo apt-get update -y
 
 echo  -e "Done: update"
 
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 echo  -e "Done: upgrade"
 
 
-sudo apt-get install openssh-server
+sudo apt-get install openssh-server -y
 
 echo  -e "Done: openssh-server installation"
 
@@ -28,17 +28,17 @@ sudo systemctl start ssh
 echo  -e "Done: ssh started"
 
 
-sudo apt-get install vim
+sudo apt-get install vim -y
 
 echo  -e "Done: vim innstalled"
 
 
-sudo apt-get install git
+sudo apt-get install git -y
 
 echo  -e "Done: git installed"
 
 
-sudo apt-get install curl
+sudo apt-get install curl -y
 
 echo  -e "Done: curl installed"
 
