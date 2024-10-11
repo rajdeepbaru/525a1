c29bd468653b8b8120086f3f3a97629625a388c2
rajdeep <212011005@iitdh.ac.in>
2024-10-11 08:43:04 +0530

    2024-10-11--0843

---
+++ cs609-autumn2024_25-/lab06-onos/script02-install-distrobox.sh
@@ -0,0 +1,44 @@
+#sudo adduser sdn
+
+#sudo usermod -aG sudo sdn
+
+#groups sdn
+#su - sdn
+#sudo ls -la /root
+
+###########
+
+
+#sudo apt-get install openjdk-11-jdk
+
+#java -version
+#############
+
+
+#sudo wget -c https://repo1.maven.org/maven2/org/onosproject/onos-releases/2.7.0/onos-2.7.0.tar.gz
+
+#sudo tar ......
+
+
+##############
+
+
+
+
+curl -O https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
+
+
+
+distrobox-list
+
+sudo apt install neofetch
+
+echo -e "run: distrobox-create --root --name ubuntuu18 --image ubuntu:18.04"
+
+echo -e "run: distrobox-enter --name debian10-distrobox"
+
+echo -e "run: cat /etc/lsb_release"
+
+echo -e "logout"
+
+
