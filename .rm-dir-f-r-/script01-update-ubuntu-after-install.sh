c7381294694c30a9279b32a0e57733816598aa9b
rajdeep <212011005@iitdh.ac.in>
2024-10-11 11:37:36 +0530

    2024-10-11--1137

--- .rm-dir-f-r-/script01-update-ubuntu-after-install.sh
+++ .rm-dir-f-r-/script01-update-ubuntu-after-install.sh
@@ -69,5 +69,5 @@ cat /home/rajdeep/.ssh/id_ed25519.pub >> /home/rajdeep/Music/key01-git
 
 cd
 
-git clone git@github.com:rajdeepbaru/525a1.git
-ssh rajdeep@10.240.60.114
+#git clone git@github.com:rajdeepbaru/525a1.git
+#ssh rajdeep@10.240.60.114
---
+++ cs609-autumn2024_25-/lab06-onos/script01-install-mininet.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/mininet/mininet
+
+sudo mininet/util/install.sh -a
+mn --version
+
