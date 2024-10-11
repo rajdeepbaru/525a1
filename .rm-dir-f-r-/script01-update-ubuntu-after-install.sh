080f944023afcf94ca8286a3d9f06581886c6475
rajdeep <212011005@iitdh.ac.in>
2024-10-11 11:39:10 +0530

    2024-10-11--1139

--- .rm-dir-f-r-/script01-update-ubuntu-after-install.sh
+++ .rm-dir-f-r-/script01-update-ubuntu-after-install.sh
@@ -69,5 +69,6 @@ cat /home/rajdeep/.ssh/id_ed25519.pub >> /home/rajdeep/Music/key01-git
 
 cd
 
-#git clone git@github.com:rajdeepbaru/525a1.git
-#ssh rajdeep@10.240.60.114
+echo -e "git clone git@github.com:rajdeepbaru/525a1.git"
+echo -e "ssh-keygen -f '/home/rajdeep/.ssh/known_hosts' -R '10.240.60.114'"
+echo -e "ssh rajdeep@10.240.60.114"
