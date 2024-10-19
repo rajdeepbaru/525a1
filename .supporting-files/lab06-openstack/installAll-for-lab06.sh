7544677e692f4d36a6335b90ad2ddbf2a9a2d2ff
rajdeep <212011005@iitdh.ac.in>
2024-10-19 14:56:33 +0530

    2024-10-19--1456

--- .supporting-files/lab06-openstack/installAll-for-lab06.sh
+++ .supporting-files/lab06-openstack/installAll-for-lab06.sh
@@ -3,13 +3,13 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 
 
-sudo apt-get install vim git curl chrony -y
-sudo apt install mariadb-server python3-pymysql -y
+
+sudo apt install chrony nova-compute python3-openstackclient mariadb-server python3-pymysql rabbitmq-server memcached python3-memcache etcd keystone glance nova-api nova-conductor nova-novncproxy nova-scheduler nova-compute vim git curl chrony -y
 
 
 
-
-
-
-
+sudo add-apt-repository cloud-archive:yoga
+sudo add-apt-repository cloud-archive:xena
+sudo add-apt-repository cloud-archive:wallaby
+sudo add-apt-repository cloud-archive:victoria
 
