a3f439f042977815187ba6e48fd270601c549343
rajdeep <212011005@iitdh.ac.in>
2024-10-20 07:32:51 +0530

    2024-10-20--0732

--- .supporting-files/lab06-openstack/installAll-for-lab06.sh
+++ .supporting-files/lab06-openstack/installAll-for-lab06.sh
@@ -4,8 +4,9 @@ sudo apt-get upgrade -y
 
 
 
-sudo apt-get install ntp chrony nova-compute python3-openstackclient mariadb-server python3-pymysql rabbitmq-server memcached python3-memcache etcd keystone glance nova-api nova-conductor nova-novncproxy nova-scheduler nova-compute vim git curl chrony net-tools -y
+sudo apt-get install ntp nova-compute python3-openstackclient mariadb-server python3-pymysql rabbitmq-server memcached python3-memcache etcd keystone glance nova-api nova-conductor nova-novncproxy nova-scheduler nova-compute vim git curl chrony net-tools -y
 
+sudo apt install chrony -y
 
 #sudo add-apt-repository cloud-archive:yoga
 #sudo add-apt-repository cloud-archive:xena
